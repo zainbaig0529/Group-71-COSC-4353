ac43c47bc5d7d1760c3261a74b08a1032a2b2223
zainbaig0529 <122945572+zainbaig0529@users.noreply.github.com>
2024-03-21 23:31:46 -0500

    changes at 11:31pm

--- app.js
+++ app.js
@@ -13,3 +13,27 @@ app.get("/", function (req, res) {
 app.listen(3000, function () {
   console.log("Server is running on localhost3000");
 });
+
+app.get("/login", function (req, res) {
+  res.sendFile(__dirname + "/public/login.html");
+});
+
+app.get("/profile_management", function (req, res) {
+  res.sendFile(__dirname + "/public/profile management.html");
+});
+
+app.get("/registration", function (req, res) {
+  res.sendFile(__dirname + "/public/registration.html");
+});
+
+app.get("/fuel_quote_history", function (req, res) {
+  res.sendFile(__dirname + "/public/fuel_quote_history.html");
+});
+
+app.get("/fuel_quote_form", function (req, res) {
+  res.sendFile(__dirname + "/public/fuel_quote_form.html");
+});
+
+app.get("/customer_info_form", function (req, res) {
+  res.sendFile(__dirname + "/public/customer_info_form.html");
+});
