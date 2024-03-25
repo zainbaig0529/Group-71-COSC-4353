420a74d6ea7afca30daee44c2bd98d6f28df298b
etpflug <148170122+etpflug@users.noreply.github.com>
2024-03-25 08:09:46 -0500

    got login backend working

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
 const express = require('express');
+var bodyParser=require("body-parser");
 const app = express();
 const PORT = 3000;
 
+//tell app where to find static content
 app.use(express.static('public'));
 app.use(express.static('assets'));
 app.use(express.static('src'));
+
+//middleware for login page
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/homepage.html");
 });
 
+app.post('/login.html', function (req, res) {
+    console.log(req.body.email);
+});
+
 app.listen(3000, function () {
   console.log("Server is running on localhost3000");
-});
-
-app.get("/login", function (req, res) {
-  res.sendFile(__dirname + "/public/login.html");
-});
-
-app.get("/profile_management", function (req, res) {
-  res.sendFile(__dirname + "/public/profile management.html");
-});
-
-app.get("/registration", function (req, res) {
-  res.sendFile(__dirname + "/public/registration.html");
-});
-
-app.get("/fuel_quote_history", function (req, res) {
-  res.sendFile(__dirname + "/public/fuel_quote_history.html");
-});
-
-app.get("/fuel_quote_form", function (req, res) {
-  res.sendFile(__dirname + "/public/fuel_quote_form.html");
-});
-
-app.get("/customer_info_form", function (req, res) {
-  res.sendFile(__dirname + "/public/customer_info_form.html");
 });
