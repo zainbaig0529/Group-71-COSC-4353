2a123f3665549554dc95cb6de84a6869baeabca6
zainbaig0529 <122945572+zainbaig0529@users.noreply.github.com>
2024-04-19 00:02:18 -0500

    changes at 12:03am

--- app.js
+++ app.js
@@ -97,27 +97,98 @@ app.post('/registration.html',
 	
 });
 
-
 app.post('/profile_management.html',
-        body('firstName').isLength({min:1, max:50}).withMessage('Invalid first name'),
-		body('lastName').isLength({min:1, max:50}).withMessage('Invalid last name'),
-		body('address1').isLength({min:1, max:100}).withMessage('Invalid address 1'),
-		body('address2').isLength({min:0, max:100}).withMessage('Invalid address 2'),
-		body('city').isLength({min:1, max:100}).withMessage('Invalid city'),
-        body('zipcode').isNumeric().isLength({min:5, max:9}).withMessage('Invalid password. Must be at least 8 characters'),
-        function (req, res) {
-    const errors = validationResult(req);
+    body('firstName').isLength({ min: 1, max: 50 }).withMessage('Invalid first name'),
+    body('lastName').isLength({ min: 1, max: 50 }).withMessage('Invalid last name'),
+    body('address1').isLength({ min: 1, max: 100 }).withMessage('Invalid address 1'),
+    body('address2').isLength({ max: 100 }).withMessage('Invalid address 2'),
+    body('city').isLength({ min: 1, max: 100 }).withMessage('Invalid city'),
+    body('state').notEmpty().withMessage('State is required'),
+    body('zipcode').isNumeric().isLength({ min: 5, max: 9 }).withMessage('Invalid zipcode. Must be between 5 and 9 digits'),
 
-    if(!errors.isEmpty())
-    {
-        return res.status(400).json(
-        {
-            success: false,
-            errors:errors.array()
+    function (req, res) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            });
+        }
+
+        const { firstName, lastName, address1, address2, city, state, zipcode } = req.body;
+
+        console.log("First Name:", firstName);
+        console.log("Last Name:", lastName);
+        console.log("Address 1:", address1);
+        console.log("Address 2:", address2);
+        console.log("City:", city);
+        console.log("State:", state);
+        console.log("Zipcode:", zipcode);
+
+        const query = "INSERT INTO ClientInformation (NameFirst, NameLast, ClientAddress1, ClientAddress2, ClientCity, ClientState, ClientZip) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        const values = [firstName, lastName, address1, address2 || null, city, state, zipcode];
+
+        database.query(query, values, function (err, result) {
+            if (err) {
+                console.error("Error inserting client information:", err);
+                return res.status(500).json({
+                    success: false,
+                    message: 'Error inserting client information'
+                });
+            }
+            console.log("Client information added successfully");
+            return res.status(200).json({
+                success: true,
+                message: 'Client information submitted successfully'
+            });
         });
-    }
+    });
 
-});
+
+
+
+/*app.post('/profile_management.html',
+    body('firstName').isLength({ min: 1, max: 50 }).withMessage('Invalid first name'),
+    body('lastName').isLength({ min: 1, max: 50 }).withMessage('Invalid last name'),
+    body('address1').isLength({ min: 1, max: 100 }).withMessage('Invalid address 1'),
+    body('address2').isLength({ min: 0, max: 100 }).withMessage('Invalid address 2'),
+    body('city').isLength({ min: 1, max: 100 }).withMessage('Invalid city'),
+    body('zipcode').isNumeric().isLength({ min: 5, max: 9 }).withMessage('Invalid zipcode. Must be between 5 and 9 digits'),
+
+    function (req, res) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            });
+        }
+
+        const { firstName, lastName, address1, address2, city, zipcode } = req.body;
+
+        // Insert user profile data into the database
+        const query = "INSERT INTO UserCredentials (FirstName, LastName, Address1, Address2, City, Zipcode) VALUES (?, ?, ?, ?, ?, ?)";
+        const values = [firstName, lastName, address1, address2, city, zipcode];
+
+        database.query(query, values, function (err, result) {
+            if (err) {
+                console.error("Error inserting user profile data:", err);
+                return res.status(500).json({
+                    success: false,
+                    message: 'Error inserting user profile data'
+                });
+            }
+            console.log("User profile data added successfully");
+            // Send response with success message
+            return res.status(200).json({
+                success: true,
+                message: 'User profile submitted successfully'
+            });
+        });
+    }); */
+
 
 app.post('/fuel_quote_form.html',
     body('GallonsRequested').isNumeric().withMessage('Gallons requested must be a number'),
