d58e9684a275a3991440d004121cad96171318b2
DavidhubOriginal <dmaranon@proton.me>
2024-04-25 03:58:01 -0500

    ugh

---
+++ views/PriceModule.js
@@ -0,0 +1,14 @@
+export const PriceModule = () =>
+        {
+            //document.getElementById("SuggestedPricePerGallon").setAttribute("value","stinky");
+            //document.getElementById('TotalAmountDue').setAttribute("value","smelly");
+            var xhttp = new XMLHttpRequest();
+            
+            xhttp.onreadystatechange = function() {
+            
+            document.getElementById("SuggestedPricePerGallon").setAttribute("value", this.responseText);
+            
+            }
+            xhttp.open("GET", "fuelQuoteMidifiedHtml.txt");
+            xhttp.send();
+        }
